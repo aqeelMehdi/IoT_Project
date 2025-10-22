b022d92285279fd4c52dfb2603b5ba19010400f3
aa08453 <aa08453@st.habib.edu.pk>
2025-10-22 11:32:26 +0500

    Initial commit for DHT sensor project

--- dht_sensor.ino
+++ dht_sensor.ino
@@ -1,24 +1,44 @@
 #include <DHT.h>
 
+#define DHT_PIN 26
+#define DHT_TYPE DHT11
+DHT dht(DHT_PIN, DHT_TYPE); 
 
-DHT dht(26, DHT11);
 void setup() {
   // put your setup code here, to run once:
-  dht.begin
-  delay(2000)
+  dht.begin();
+  delay(2000);
 
-  Serial.begin(115200)
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   float temp=dht.readTemperature();
   float humid=dht.readHumidity();
-  Serial.print("Temp: ");
-  Serial.print(temp);
-  Serial.print(" C ");
-  Serial.print("Humidity: ");
-  Serial.print(humid);
-  Serial.print(" % ");
-  delay(2000);
+  Serial.println("------ SENSOR READINGS ------"); 
+
+
+  if (isnan(temp) || isnan(humid)) { 
+
+  Serial.println("Temp & Humidity: Error reading from DHT11"); 
+
+  } else { 
+
+  Serial.print("Temp: "); 
+
+  Serial.print(temp); 
+
+  Serial.println(" C"); 
+
+  
+
+  Serial.print("Humidity: "); 
+
+  Serial.print(humid); 
+
+  Serial.println(" %"); 
+
+  delay(2000);
+}
 }
